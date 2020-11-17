07686937e7809b3bbcc643d6561195bc96e1639d
Carlos Andres Rojas Parra <carlos.rojasp@autonoma.edu.co>
2020-11-17 09:44:43 -0500

    Api route Auth init

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function login(Request $request){
+
+        if($request->email){
+            $user = User::where('email','=',$request->email)->first();
+            if($user && Hash::check($request->password, $user->password)){
+                
+                $token = $user->createToken('simulador')->accessToken;
+
+                return response()->json([
+                    'res'=>true,
+                    'token'=>$token,
+                    'message'=>'success'
+                ],200);
+            }else{
+                return response()->json([
+                    'res'=>false,
+                    'message'=>'Wrong email or password'
+                ],200); 
+            }
+        }else{
+            return response()->json([
+                'res'=>false,
+                'message'=>'Wrong data'
+            ],200); 
+        }
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'Api\UserController@login');
+
+Route::get('prueba', function () {
+    return response()->json([
+        'res'=>true,
+        'message'=>'Hola mundo te saludo desde laravel'
+    ],200); 
+});
