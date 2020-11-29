1811dfcebe3667ca7f08e487f565b6322ac559aa
Mariano Andres Medina Duque <bruder1888@gmail.com>
2020-11-29 10:10:23 -0500

    Readme update

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\SimuladoBiomedicaBack\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\SimuladoBiomedicaBack\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\simuladorbiomedicabackend\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\simuladorbiomedicabackend\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
