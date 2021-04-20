4ff5f55005bfb786aae5b005e726481a24bc2781
Carlos Andres Rojas Parra <carlos.rojasp@autonoma.edu.co>
2021-04-19 23:38:44 -0500

    Register test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,12 +12,40 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="fullname" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="fullname" type="text" class="form-control @error('fullname') is-invalid @enderror" name="fullname" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
+                                @error('fullname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="career" class="col-md-4 col-form-label text-md-right">{{ __('Carrera') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="career" type="text" class="form-control @error('career') is-invalid @enderror" name="career" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('career')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="semester" class="col-md-4 col-form-label text-md-right">{{ __('Semester') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="semester" type="text" class="form-control @error('semester') is-invalid @enderror" name="semester" value="{{ old('semester') }}" required autocomplete="name" autofocus>
+
+                                @error('semester')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
