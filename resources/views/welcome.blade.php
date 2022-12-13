f0568a248e05a6362ec0a6f16a3a55073eefbe0f
Carlos Andrés Rojas Parra <c.andresr31@gmail.com>
2022-12-12 21:05:07 -0500

    Test vercel deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,9 +21,9 @@
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet"> --}}
 
     <link href=" {{ asset('css/welcome/style.css') }}" rel="stylesheet">
 </head>
